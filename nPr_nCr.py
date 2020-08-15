d93a0e098ddbeb00a6a8f899fee6fb61c1cc718e
AAAAAAAHSAN <bangladeshahsan@gmail.com>
2020-08-15 16:22:37 +0600

    Changed the font size

--- nPr_nCr.py
+++ nPr_nCr.py
@@ -1,5 +1,4 @@
 from tkinter import *
-from tkinter.messagebox import *
 
 
 def fact():
@@ -32,9 +31,10 @@ def ncr():
 
 main_window = Tk()
 main_window.geometry('500x600')
-Label(main_window, text="Enter the value of n").grid(row=0)
-Label(main_window, text="Enter the value of r").grid(row=1)
-Label(main_window, text="The Answer is:").grid(row=2)
+main_window.configure(background='green')
+Label(main_window, text="Enter the value of n", font=("Arial Bold", 15)).grid(row=0)
+Label(main_window, text="Enter the value of r", font=("Arial Bold", 15)).grid(row=1)
+Label(main_window, text="The Answer is:", font=("Arial Bold", 15)).grid(row=2)
 
 num1 = Entry(main_window)
 num2 = Entry(main_window)
@@ -44,9 +44,9 @@ num1.grid(row=0, column=1)
 num2.grid(row=1, column=1)
 ans_field.grid(row=2, column=1)
 
-Button(main_window, text='Quit', command=main_window.destroy).grid(row=4, column=0, sticky=W, pady=4)
-Button(main_window, text='nCr', command=ncr).grid(row=5, column=4, sticky=W, pady=4)
-Button(main_window, text='nPr', command=npr).grid(row=5, column=1, sticky=W, pady=4)
-Button(main_window, text='factorial', command=fact).grid(row=5, column=0, sticky=W, pady=4)
+Button(main_window, text='Quit', command=main_window.destroy, font=("Arial Bold", 10)).grid(row=4, column=7, sticky=W, pady=4)
+Button(main_window, text='nCr', command=ncr, font=("Arial Bold", 10)).grid(row=5, column=4, sticky=W, pady=4)
+Button(main_window, text='nPr', command=npr, font=("Arial Bold", 10)).grid(row=5, column=1, sticky=W, pady=4)
+Button(main_window, text='n!', command=fact, font=("Arial Bold", 10)).grid(row=5, column=0, sticky=W, pady=4)
 
 mainloop()
